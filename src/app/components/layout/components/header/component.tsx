bb5a2c31ad9ac4352c9aff69a49fe866d5e7152c
Roe Greene <robert@byrojo.com>
2022-03-16 02:52:01 -0500

    make nav better

--- src/app/components/layout/components/header/component.tsx
+++ src/app/components/layout/components/header/component.tsx
@@ -1,18 +1,33 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Link } from "react-router-dom"
 import './styles.scss'
+import { useInView } from 'react-intersection-observer'
 
 const Header: React.FC = () => {
+  const { ref, inView } = useInView()
+
+  useEffect( () => {
+    if( !inView ) {
+      document.body.classList.add( "nav-shadow" )
+    } else {
+      document.body.classList.remove( "nav-shadow" )
+    }
+  }, [ inView ] )
+
   return (
-    <header className="app-nav">
-      <nav>
-        <Link to="/" aria-label="Logo">
-          <h1 className="app-logo">
+    <>
+      
+      <header className="app-nav">
+        <nav>
+          <Link to="/" aria-label="Logo">
+            <h1 className="app-logo">
             • Roe <small>the</small> Dev •
-          </h1>
-        </Link>
-      </nav>
-    </header>
+            </h1>
+          </Link>
+        </nav>
+      </header>
+      <span ref={ref} className="nav-observer" />
+    </>
   )
 }
 
