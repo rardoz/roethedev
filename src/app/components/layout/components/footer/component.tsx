a629148921da1f6d477f737fa3d09c0474c8ff09
Roe Greene <robert@byrojo.com>
2022-03-17 00:41:40 -0500

    style footer

--- src/app/components/layout/components/footer/component.tsx
+++ src/app/components/layout/components/footer/component.tsx
@@ -5,17 +5,19 @@ import Section from '../../../section'
 
 const Footer: React.FC = () => {
   return (
-    <WaveContainer type="bottom">
-      <footer className="app-footer d-flex">
-        <nav className="d-flex">
-          <Section className="section-wider d-flex">
-            <div className="d-flex flex-end align-self-center">
+    <div className="app-footer">
+      <WaveContainer type="bottom">
+        <footer className=" d-flex">
+          <nav className="d-flex">
+            <Section className="section-wider d-flex">
+              <div className="d-flex flex-end align-self-center">
             &copy; 2022.&nbsp;<a href="https://byrojo.com" target="_blank">Find more at byrojo.com</a>.
-            </div>
-          </Section>
-        </nav>
-      </footer>
-    </WaveContainer>
+              </div>
+            </Section>
+          </nav>
+        </footer>
+      </WaveContainer>
+    </div>
   )
 }
 
