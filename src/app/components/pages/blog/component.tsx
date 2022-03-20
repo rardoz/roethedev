36c8ace021475417bcf1401397fc49c37d531b8b
Roe Greene <robert@zardozcs.com>
2022-03-20 15:26:08 -0500

    add blog pages (#18)

---
+++ src/app/components/pages/blog/component.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { CMSContextProvider } from '../../../context/cms'
+import WaveHero from '../../wave-hero'
+import WaveHeroTitle from '../../wave-hero/components/wave-hero-title'
+import BlogFeed from '../home/components/blog-feed'
+import { Helmet } from "react-helmet-async"
+import './styles.scss'
+
+const BlogPage: React.FC = () => {
+  return (
+    <>
+      <Helmet>
+        <title>From my desk | Blog</title>
+        <meta name='description' content="From by desk is my blog. I talk about art, tech, and my opinions as well as share tutorials and videos." />
+      </Helmet>
+      <WaveHero>
+        <WaveHeroTitle title1='From' title2='my' title3='desk' />
+      </WaveHero>
+      <CMSContextProvider limit={20} contentType={process.env.CONTENTFUL_BLOG_ID}>
+        <BlogFeed className='blog-page-blog-feed' linkEnabled={false} enableTitle={false} />
+      </CMSContextProvider>
+    </>
+  )
+}
+
+export default BlogPage
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react'
 import { render } from 'react-dom'
+import { HelmetProvider, Helmet } from "react-helmet-async"
 import {
   BrowserRouter,
   Routes,
@@ -9,18 +10,30 @@ import Layout from './components/layout'
 
 const HomePage = React.lazy( () => import( './components/pages/home' ) )
 const AboutPage = React.lazy( () => import( './components/pages/about' ) )
+const BlogPage = React.lazy( () => import( './components/pages/blog' ) )
+
+const PortfolioPage = React.lazy( () => import( './components/pages/portfolio' ) )
 
 const Application: React.FC = () => (
-  <BrowserRouter>
-    <Layout>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />}/>
-        </Routes>
-      </Suspense>
-    </Layout>
-  </BrowserRouter>
+  <HelmetProvider>
+    <Helmet>
+      <title>Roe the Dev</title>
+      <meta name="description"
+        content="Javascript Web and Native Mobile Engineer available for consulting, projects, and tutoring." />
+    </Helmet>
+    <BrowserRouter>
+      <Layout>
+        <Suspense fallback={<div>...</div>}>
+          <Routes>
+            <Route index element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />}/>
+            <Route path="/blog" element={<BlogPage />}/>
+            <Route path="/portfolio" element={<PortfolioPage />}/>
+          </Routes>
+        </Suspense>
+      </Layout>
+    </BrowserRouter>
+  </HelmetProvider>
 )
 
 render( <Application />, document.getElementById( 'root' ) )
