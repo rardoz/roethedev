9f460a3da2f54d1082e7969c4a3dcae762cd7957
Roe Greene <robert@byrojo.com>
2022-03-21 02:04:57 -0500

    scroll to top on route change

--- src/app/components/layout/component.tsx
+++ src/app/components/layout/component.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Footer from './components/footer'
 
+import { useLocation } from 'react-router-dom'
+
 const Layout: React.FC = ( { children } ) => {
+  const location = useLocation()
+  
+  useEffect( () => {
+    window.scrollTo( { top: 0 } )
+  }, [ location ] )
+
   return (
     <>
       {children}
