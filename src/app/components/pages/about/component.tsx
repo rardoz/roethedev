51671c5f9122ccfc98ba5408fa957cf15da6edc4
Roe Greene <robert@byrojo.com>
2022-03-21 01:29:26 -0500

    fix bugs

--- src/app/components/pages/about/component.tsx
+++ src/app/components/pages/about/component.tsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import AppLink from '../../app-link'
 import Header from '../../layout/components/header'
-
+import Section from '../../section'
+import SectionTitle from '../../section-title'
+import BigAvatar from '../home/components/about-section/components/big-avatar'
+import ContentBlock from '../home/components/about-section/components/content-block'
+import avatarImg from '../home/components/about-section/assets/roe-greene.png'
+import avatarImgAlt from '../home/components/about-section/assets/roe-greene-alt.png'
+import WaveContainer from '../../wave-container'
 const AboutPage: React.FC = () => {
   return (
     <>
       <Header /> 
-      <div>
-            Hello about page!!
-      </div>
+      <WaveContainer>
+        <h1 className='text-center'>About Roe</h1>
+      </WaveContainer>
+      <br />
+      <br /> 
+      <Section>
+        <div className="flex-grid space-between">
+          <BigAvatar avatarImg={avatarImg} avatarImgAlt={avatarImgAlt} />
+          <ContentBlock className="col-5"  />
+        </div>
+        <br/>
+        <br/>
+        <div className="d-flex flex-center app-link-container">
+          <a className='app-link' href="https://roe-files.s3.amazonaws.com/Roe-Greene-Resume.pdf" download><h2>Download my resume</h2></a>
+        </div>
+      </Section>
+  
+      <br />
     </>
   )
 }
