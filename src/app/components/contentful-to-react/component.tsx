b00b9dde927dae28035292c05fcf3aa918ada14f
Roe Greene <robert@byrojo.com>
2022-03-22 00:23:59 -0500

    style contentful content

--- src/app/components/contentful-to-react/component.tsx
+++ src/app/components/contentful-to-react/component.tsx
@@ -1,19 +1,32 @@
-import { BLOCKS, MARKS, Document, Block, Inline } from '@contentful/rich-text-types'
+import { BLOCKS, MARKS, INLINES, Document, Block, Inline } from '@contentful/rich-text-types'
 import { documentToReactComponents, } from '@contentful/rich-text-react-renderer'
 import React from 'react'
+import './styles.scss'
 
 const options = {
   renderMark: {
     [ MARKS.BOLD ]: ( text:React.ReactNode ) => <strong>{text}</strong>,
   },
   renderNode: {
+    [ INLINES.HYPERLINK ]: ( node: Block | Inline, children: React.ReactNode ) => <a href={node.data.uri} target="_blank">{children}</a>,
     [ BLOCKS.PARAGRAPH ]: ( node: Block | Inline, children: React.ReactNode ) => <p>{children}</p>,
   },
-  renderText: ( text:string ) => text.replace( '!', '?' ),
+  renderText: ( text: string ) => {
+    try {
+      return text.split( '\n' )
+        .flatMap( ( sentence, i ) => [
+          i > 0 && <br />,
+          sentence.startsWith( "<iframe " )
+            ? <div dangerouslySetInnerHTML={{ __html: sentence }} /> :  sentence ] )
+    } catch( e ) {
+      console.info( e )
+      return text
+    }
+  }
 }
 
 const ContentfulToReact: React.FC<{content: Document}> = ( { content } ) => (
-  <>{documentToReactComponents( content, options )}</>
+  <div className='contentful-to-react'>{documentToReactComponents( content, options )}</div>
 )
 
 export default ContentfulToReact
