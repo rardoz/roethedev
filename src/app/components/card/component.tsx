3e09d5c0b872eafcacad10b00f6b5013691a1ec8
Roe Greene <robert@byrojo.com>
2022-03-22 01:51:44 -0500

    add posts

--- src/app/components/card/component.tsx
+++ src/app/components/card/component.tsx
@@ -4,27 +4,38 @@ import LazyImage from '../lazy-image'
 import { Link } from 'react-router-dom'
 import './styles.scss'
 
+const commonProps =    {
+  tabIndex:1,
+  className: "card-link-wrapper" 
+}
+
 const Card: React.FC<{
     className?: string
     title: string
     description: string
     to: string
     featuredImage: string
-}> = ( { className, description, to, title, featuredImage } ) => {
+    hardRoute?: boolean
+}> = ( { className, description, to, title, featuredImage, hardRoute } ) => {
+
+  const linkContent = ( <>
+    <div className='card-image'>
+      <LazyImage imgSrc={featuredImage}  />
+    </div>
+    <div className='card-content'>
+      <h3 className='clamp-title'>{title}</h3>
+      <div className='clamp-content'>
+        <p>{description}</p>
+      </div>
+    </div>
+    <small className='read'>Read</small>
+  </> )
+
   return ( 
     <div className={classnames( 'card', className )}>
-      <Link to={to} tabIndex={1} className="card-link-wrapper">
-        <div className='card-image'>
-          <LazyImage imgSrc={featuredImage}  />
-        </div>
-        <div className='card-content'>
-          <h3 className='clamp-title'>{title}</h3>
-          <div className='clamp-content'>
-            <p>{description}</p>
-          </div>
-        </div>
-        <small className='read'>Read</small>
-      </Link>
+      {hardRoute ? <a {...commonProps} href={to} target="_blank">{linkContent}</a> : <Link to={to} tabIndex={1} className="card-link-wrapper">
+        {linkContent}
+      </Link>}
     </div>
   )
 }
