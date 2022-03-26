92fbbc5fe0e7640758dfe6e3aee005405544cadd
Roe Greene <robert@byrojo.com>
2022-03-25 19:55:45 -0500

    upgrade to ga 4

--- src/app/analytics/index.ts
+++ src/app/analytics/index.ts
@@ -1,23 +1,25 @@
-import ReactGA, { EventArgs } from "react-ga"
-
-const GA_DEBUG = process.env.GA_DEBUG
+import ReactGA from "react-ga4"
+import { UaEventOptions } from 'react-ga4/types/ga4'
 const GA_TRACKING_ID = process.env.GA_TRACKING_ID
 
 const init = ():void => {
   if( GA_TRACKING_ID ) {
-    ReactGA.initialize( GA_TRACKING_ID, { debug: `${GA_DEBUG}`.toLowerCase() === "true" } )
+    ReactGA.initialize( GA_TRACKING_ID )
   } else {
     console.error( "GA_TRACKING_ID env variable not set" )
   }
 }
 
-const sendEvent = ( payload: EventArgs ):void => {
-  ReactGA.event( payload )
+const sendEvent = ( payload: UaEventOptions ):void => {
+  ReactGA.event( {
+    nonInteraction: true,
+    ...payload
+  } )
 }
 
 const sendPageview = ( path: string ):void => {
   ReactGA.set( { page: path } )
-  ReactGA.pageview( path )
+  ReactGA.send( { hitType: "pageview", page: path } )
 }
 
 export default {
